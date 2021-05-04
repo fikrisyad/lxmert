81ec36adb9b525fa98bc67b70b5679380d378922
fikrisyad <abdurrisyadfikri@gmail.com>
2021-05-04 14:36:22 +0900

    updating the bash scripts

--- run/vcsd_finetune.bash
+++ run/vcsd_finetune.bash
@@ -12,5 +12,6 @@ CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/vcsd.py \
     --train train --valid val  \
     --llayers 9 --xlayers 5 --rlayers 5 \
-    --batchSize 32 --optim bert --lr 5e-5 --epochs 4 \
+    --batchSize 32 --optim bert --lr 5e-5 --epochs 10 \
+    --resizeImage 1 \
     --tqdm --output $output ${@:3}
--- run/vcsd_test.bash
+++ run/vcsd_test.bash
@@ -13,4 +13,5 @@ CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
     --tiny --train train --valid ""  \
     --llayers 9 --xlayers 5 --rlayers 5 \
     --batchSize 32 --optim bert --lr 5e-5 --epochs 4 \
+    --resizeImage 1 \
     --tqdm --output $output ${@:3}
