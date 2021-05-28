c37b58a3b4932f52e0b27ecedd862d34fa72265d
fikrisyad <abdurrisyadfikri@gmail.com>
2021-05-28 20:16:00 +0900

    apply the new dataset

--- src/tasks/vcsd_bbox.py
+++ src/tasks/vcsd_bbox.py
@@ -17,8 +17,10 @@ DataTuple = collections.namedtuple("DataTuple", 'dataset loader evaluator')
 
 
 def get_data_tuple(splits: str, bs: int, shuffle=False, drop_last=False, resize_img=False) -> DataTuple:
-    dset = VCSDDataset(splits)
-    tset = VCSDTorchDataset(dset, resize_img)
+    # dset = VCSDDataset(splits)
+    dset = VCSDDatasetVGRegions
+    # tset = VCSDTorchDataset(dset, resize_img)
+    tset = VCSDTorchDatasetVGRegions(dset, resize_img)
     evaluator = VCSDEvaluator(dset)
     data_loader = DataLoader(
         tset, batch_size=bs,
